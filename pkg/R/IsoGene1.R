9c9581398b957e1b6b7012ea2d8581cb4e6a652d
danlin <danlin@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-02-04 13:56:49 +0000

    remove old pava function
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/isogene@115 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/IsoGene1.R
+++ pkg/R/IsoGene1.R
@@ -1,31 +1,3 @@
-#pava <- function (x, wt=rep(1,length(x)))
-#  Compute the isotonic regression of numeric vector 'x', with
-#  weights 'wt', with respect to simple order.  The pool-adjacent-
-#  violators algorithm is used.  Returns a vector of the same length
-#  as 'x' containing the regression.
-
-#  02 Sep 1994 / R.F. Raubertas
-#{
-#   n <- length(x)
-#   if (n <= 1) return (x)
-#   if (any(is.na(x)) || any(is.na(wt))) {
-#      stop ("Missing values in 'x' or 'wt' not allowed")
-#   }
-#   lvlsets <- (1:n)
-#   repeat {
-#      viol <- (as.vector(diff(x)) < 0)  # Find adjacent violators
-#      if (!(any(viol))) break
-#
-#      i <- min( (1:(n-1))[viol])        # Pool first pair of violators
-#      lvl1 <- lvlsets[i]
-#      lvl2 <- lvlsets[i+1]
-#      ilvl <- (lvlsets == lvl1 | lvlsets == lvl2)
-#      x[ilvl] <- sum(x[ilvl]*wt[ilvl]) / sum(wt[ilvl])
-#      lvlsets[ilvl] <- lvl1
-#   }
-#  return(x)
-#}
-
 
 IsoGene1 <- function(x, y){
   ordx <- order(x)
@@ -41,10 +13,9 @@ IsoGene1 <- function(x, y){
   y.m.tot <- rep(mean(y), length(unx))
   
    y.is.u <- pava(y = y.m, w = n.p )
-#   y.is.d <- rev(pava(y = rev(y.m), w = rev(n.p)))
+
    y.is.d <- pava(y = y.m, w = n.p, decreasing=TRUE)
-##  y.is.u <- isoreg(unx,  y.m)$yf          ##equal weights
-##  y.is.d <- rev(isoreg(rev(unx), y.m)$yf)
+
   
 
 
